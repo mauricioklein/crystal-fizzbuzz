4a2f156a85b45c5d484d7dfaf5bdd8af5ee40c1d
Mauricio Klein <mauricio.klein@jura-online.de>
2017-02-03 11:47:26 +0100

    Added support for FizzBuzz of range of values

--- spec/fizzbuzz_spec.cr
+++ spec/fizzbuzz_spec.cr
@@ -3,6 +3,11 @@ require "./spec_helper"
 Spec2.describe Fizzbuzz do
   subject { Fizzbuzz.new }
 
+  let(fizz)     { "Fizz" }
+  let(buzz)     { "Buzz" }
+  let(fizzbuzz) { "FizzBuzz" }
+
+
   context "class structure" do
     it "should instantiate the class" do
       expect(subject).to be_a(Fizzbuzz)
@@ -10,28 +15,45 @@ Spec2.describe Fizzbuzz do
   end
 
   context "single value" do
-    it "should return 'fizz' for multiples of 3" do
-      expect(subject.fizzbuzz_of(3)).to eq("Fizz")
-      expect(subject.fizzbuzz_of(6)).to eq("Fizz")
-      expect(subject.fizzbuzz_of(9)).to eq("Fizz")
+    context "Fizz" do
+      it "should return 'fizz' for multiples of 3" do
+        expect(subject.fizzbuzz_of(3)).to eq(fizz)
+        expect(subject.fizzbuzz_of(6)).to eq(fizz)
+        expect(subject.fizzbuzz_of(9)).to eq(fizz)
+      end
     end
 
-    it "should return 'Buzz' for multiples of 5" do
-      expect(subject.fizzbuzz_of(5)).to eq("Buzz")
-      expect(subject.fizzbuzz_of(10)).to eq("Buzz")
-      expect(subject.fizzbuzz_of(50)).to eq("Buzz")
+    context "Buzz" do
+      it "should return 'Buzz' for multiples of 5" do
+        expect(subject.fizzbuzz_of(5)).to  eq(buzz)
+        expect(subject.fizzbuzz_of(10)).to eq(buzz)
+        expect(subject.fizzbuzz_of(50)).to eq(buzz)
+      end
     end
 
-    it "should return 'Fizzbuzz' for multiples of 3 and 5" do
-      expect(subject.fizzbuzz_of(15)).to eq("Fizzbuzz")
-      expect(subject.fizzbuzz_of(30)).to eq("Fizzbuzz")
-      expect(subject.fizzbuzz_of(45)).to eq("Fizzbuzz")
+    context "FizzBuzz" do
+      it "should return 'Fizzbuzz' for multiples of 3 and 5" do
+        expect(subject.fizzbuzz_of(15)).to eq(fizzbuzz)
+        expect(subject.fizzbuzz_of(30)).to eq(fizzbuzz)
+        expect(subject.fizzbuzz_of(45)).to eq(fizzbuzz)
+      end
     end
 
-    it "should return the number itself if not multiple of 3 and 5" do
-      expect(subject.fizzbuzz_of(1)).to eq(1)
-      expect(subject.fizzbuzz_of(2)).to eq(2)
-      expect(subject.fizzbuzz_of(4)).to eq(4)
+    context "default case" do
+      it "should return the number itself if not multiple of 3 and 5" do
+        expect(subject.fizzbuzz_of(1)).to eq(1)
+        expect(subject.fizzbuzz_of(2)).to eq(2)
+        expect(subject.fizzbuzz_of(4)).to eq(4)
+      end
+    end
+  end
+
+  context "range" do
+    let(range) { (5..10) }
+    let(expeceted_response) { [buzz, fizz, 7, 8, fizz, buzz] }
+
+    it "should calculate all the fizzbuzz for values in given range" do
+      expect(subject.fizzbuzz_of(range)).to eq(expeceted_response)
     end
   end
 end
--- src/fizzbuzz.cr
+++ src/fizzbuzz.cr
@@ -3,9 +3,15 @@ class Fizzbuzz
     divisible_by_3 = number.divisible_by?(3)
     divisible_by_5 = number.divisible_by?(5)
 
-    return "Fizzbuzz" if (divisible_by_3 && divisible_by_5)
+    return "FizzBuzz" if (divisible_by_3 && divisible_by_5)
     return "Fizz"     if (divisible_by_3)
     return "Buzz"     if (divisible_by_5)
     return number
+  end
+
+  def fizzbuzz_of(range : Range(Int32, Int32))
+    range.map do |number|
+      fizzbuzz_of(number)
+    end
   end
 end
