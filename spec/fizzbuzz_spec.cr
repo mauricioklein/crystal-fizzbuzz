0610dc225b8fab47668c028d8d5a90eb19c59745
Mauricio Klein <mauricio.klein@jura-online.de>
2017-02-03 12:34:48 +0100

    Refactoring

--- spec/fizzbuzz_spec.cr
+++ spec/fizzbuzz_spec.cr
@@ -1,59 +1,52 @@
 require "./spec_helper"
 
 Spec2.describe Fizzbuzz do
-  subject { Fizzbuzz.new }
+  subject { Fizzbuzz }
 
   let(fizz)     { "Fizz" }
   let(buzz)     { "Buzz" }
   let(fizzbuzz) { "FizzBuzz" }
 
-
-  context "class structure" do
-    it "should instantiate the class" do
-      expect(subject).to be_a(Fizzbuzz)
-    end
-  end
-
   context "single value" do
     context "Fizz" do
       it "should return 'fizz' for multiples of 3" do
-        expect(subject.fizzbuzz_of(3)).to eq(fizz)
-        expect(subject.fizzbuzz_of(6)).to eq(fizz)
-        expect(subject.fizzbuzz_of(9)).to eq(fizz)
+        expect(subject.of(3)).to eq(fizz)
+        expect(subject.of(6)).to eq(fizz)
+        expect(subject.of(9)).to eq(fizz)
       end
     end
 
     context "Buzz" do
       it "should return 'Buzz' for multiples of 5" do
-        expect(subject.fizzbuzz_of(5)).to  eq(buzz)
-        expect(subject.fizzbuzz_of(10)).to eq(buzz)
-        expect(subject.fizzbuzz_of(50)).to eq(buzz)
+        expect(subject.of(5)).to  eq(buzz)
+        expect(subject.of(10)).to eq(buzz)
+        expect(subject.of(50)).to eq(buzz)
       end
     end
 
     context "FizzBuzz" do
       it "should return 'Fizzbuzz' for multiples of 3 and 5" do
-        expect(subject.fizzbuzz_of(15)).to eq(fizzbuzz)
-        expect(subject.fizzbuzz_of(30)).to eq(fizzbuzz)
-        expect(subject.fizzbuzz_of(45)).to eq(fizzbuzz)
+        expect(subject.of(15)).to eq(fizzbuzz)
+        expect(subject.of(30)).to eq(fizzbuzz)
+        expect(subject.of(45)).to eq(fizzbuzz)
       end
     end
 
     context "default case" do
       it "should return the number itself if not multiple of 3 and 5" do
-        expect(subject.fizzbuzz_of(1)).to eq(1)
-        expect(subject.fizzbuzz_of(2)).to eq(2)
-        expect(subject.fizzbuzz_of(4)).to eq(4)
+        expect(subject.of(1).to_s).to eq("1")
+        expect(subject.of(2).to_s).to eq("2")
+        expect(subject.of(4).to_s).to eq("4")
       end
     end
   end
 
   context "range" do
     let(range) { (5..10) }
-    let(expeceted_response) { [buzz, fizz, 7, 8, fizz, buzz] }
+    let(expeceted_response) { [buzz, fizz, 7, 8, fizz, buzz].join(", ") }
 
     it "should calculate all the fizzbuzz for values in given range" do
-      expect(subject.fizzbuzz_of(range)).to eq(expeceted_response)
+      expect(subject.of(range)).to eq(expeceted_response)
     end
   end
 end
--- src/fizzbuzz.cr
+++ src/fizzbuzz.cr
@@ -1,17 +1,23 @@
-class Fizzbuzz
-  def fizzbuzz_of(number : Number)
+module Fizzbuzz
+  extend self
+
+  def calculate_fizzbuzz_of(number : Number)
     divisible_by_3 = number.divisible_by?(3)
     divisible_by_5 = number.divisible_by?(5)
 
-    return "FizzBuzz" if (divisible_by_3 && divisible_by_5)
-    return "Fizz"     if (divisible_by_3)
-    return "Buzz"     if (divisible_by_5)
-    return number
+    case
+    when (divisible_by_3 && divisible_by_5) then "FizzBuzz"
+    when (divisible_by_3) then "Fizz"
+    when (divisible_by_5) then "Buzz"
+    else number
+    end
   end
 
-  def fizzbuzz_of(range : Range(Int32, Int32))
-    range.map do |number|
-      fizzbuzz_of(number)
-    end
+  def of(number : Number)
+    calculate_fizzbuzz_of(number).to_s
+  end
+
+  def self.of(range : Range(Int32, Int32))
+    range.map { |number| calculate_fizzbuzz_of(number) }.join(", ")
   end
 end
